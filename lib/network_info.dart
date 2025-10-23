0347fa832c9b9b4baaff5e4653d218cca2c7c8d7
SepehrTabeian <sepehrtabeian@gmail.com>
2025-10-23 08:17:04 +0530

    chore: prepare for pub.dev publication

--- lib/network_info.dart
+++ lib/network_info.dart
@@ -4,13 +4,16 @@
 /// Author: Sepehr Tabeian
 /// License: MIT
 /// Copyright (c) 2025 Sepehr Tabeian
-library network_info;
+
+// Dependency injection
+import 'package:network_info/src/di/network_info_di.dart';
+import 'package:network_info/src/domain/repository/i_network_info_repository.dart';
 
 // Data layer exports (only config, not implementations)
 export 'src/data/config/network_info_config.dart';
-// Dependency injection
-export 'src/di/network_info_di.dart';
-// Domain layer exports
-export 'src/domain/exceptions/network_info_exception.dart';
 export 'src/domain/models/network_info_model.dart';
-export 'src/domain/repository/i_network_info_repository.dart';
+
+typedef NetworkInfo = INetworkInfoRepository;
+
+/// Global instance of [NetworkInfoDI] for easy access
+final networkInfoDI = NetworkInfoDI();
